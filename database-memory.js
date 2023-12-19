737e49f422d8ee747eb3c1dfbf5bfad2b33d93d2
Iwaerley <154382159+Iwaerley@users.noreply.github.com>
2023-12-19 18:29:59 -0400

    RECUPERAÇÃO

---
+++ database-memory.js
@@ -0,0 +1,43 @@
+import { randomUUID } from "crypto"
+
+export class DatabaseMemory{
+#trechoaereos = new Map()
+
+//listando trechoaereos em as  chaves
+list(search){
+    return Array.from(this.#trechoaereos.entries()).map((trechoaereoArray)  => {
+        //Primeira Posição
+        const id = trechoaereoArray[0]
+        //Segunda Posição
+        const data = trechoaereoArray[1]
+
+        return{
+            id,
+            ...data,
+        }
+    })
+//Retornando apenas resultados da pesquisa
+.filter(trechoaereo => {
+    if (search) {
+        return trechoaereo.País.includes(search)
+    }
+    return true
+})
+}
+
+//Estou criando um trechoaereo
+create(trechoaereo){
+    // Gerando id de trechoaereos aleatorio
+    const trechoaereoId = randomUUID()
+    this.#trechoaereos.set(trechoaereoId, trechoaereo)
+}
+
+//Atualizando o trechoaereo
+update(id, trechoaereo){
+    this.#trechoaereos.set(id, trechoaereo)
+}
+//Deletando livro
+delete(id, trechoaereo){
+    this.#trechoaereos.delete(id, trechoaereo)
+}
+}
